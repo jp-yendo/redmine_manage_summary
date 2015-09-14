5f6212d00d665d2b6343986541245fc395bdeb5b
yendo <yendo@localhost.localdomain>
2015-09-14 13:35:52 +0900

    add Progress summary

--- app/controllers/progress_summary_controller.rb
+++ app/controllers/progress_summary_controller.rb
@@ -1,9 +1,13 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+class ProgressSummaryController < ApplicationController
 
-class ProgressSummaryController
-  def initialize
-    
+  def index
+    @filters_message = ""
+
+    render "show"
+  end
+
+  def show
+    @filters_message = ""
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 RedmineApp::Application.routes.draw do
-  match 'time_summary/:action', :to => 'time_summary#index', :via => [:get, :post], as: 'managesummary_top_route'
-  match 'time_summary/:action/:id', :to => 'time_summary#show', :via => [:get, :post], as: 'managesummary_project_route'
+  match 'time_summary/:action', :to => 'time_summary#index', :via => [:get, :post], as: 'manage_time_summary_top_route'
+  match 'time_summary/:action/:id', :to => 'time_summary#show', :via => [:get, :post], as: 'manage_time_summary_project_route'
+
+  match 'progress_summary/:action', :to => 'progress_summary#index', :via => [:get, :post], as: 'manage_progress_summary_top_route'
+  match 'progress_summary/:action/:id', :to => 'progress_summary#show', :via => [:get, :post], as: 'manage_progress_summary_project_route'
 end
