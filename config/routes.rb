5c6e063d7208fa5bd3006d24e366e31ac056869c
yendo <yendo@localhost.localdomain>
2015-11-11 14:30:06 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
-RedmineApp::Application.routes.draw do
-  match 'time_summary/:action', :to => 'time_summary#index', :via => [:get, :post], as: 'manage_time_summary_top_route'
-  match 'time_summary/:action/:id', :to => 'time_summary#show', :via => [:get, :post], as: 'manage_time_summary_project_route'
+match 'projects/time_summary/:action', :controller => 'time_summary', :via => [:get, :post], as: 'manage_time_summary_top_route'
+match 'projects/:id/time_summary/:action', :controller => 'time_summary', :via => [:get, :post], as: 'manage_time_summary_project_route'
 
-  match 'progress_summary/:action', :to => 'progress_summary#index', :via => [:get, :post], as: 'manage_progress_summary_top_route'
-  match 'progress_summary/:action/:id', :to => 'progress_summary#show', :via => [:get, :post], as: 'manage_progress_summary_project_route'
-end
+match 'projects/progress_summary/:action', :controller => 'progress_summary', :via => [:get, :post], as: 'manage_progress_summary_top_route'
+match 'projects/:id/progress_summary/:action', :controller => 'progress_summary', :via => [:get, :post], as: 'manage_progress_summary_project_route'
